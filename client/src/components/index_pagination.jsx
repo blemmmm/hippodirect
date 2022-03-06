d97b76944d6359736b2098c5d5c0e1a21acd23de
Blessly Pera <pera.blessly@yahoo.com>
2022-03-06 21:43:15 +0800

    ui fix

--- client/src/components/index_pagination.jsx
+++ client/src/components/index_pagination.jsx
@@ -64,10 +64,10 @@ function Index_Pagination (props) {
   return (
     <nav className="container mx-auto px-6 py-2">
       <ul className="flex flex-row justify-end">
-        <li><button onClick={prev_page} className="mx-1 h-10 px-5 text-indigo-600 transition-colors duration-150 rounded-l focus:shadow-outline bg-indigo-100">
+        <li><button onClick={prev_page} className="mx-1 h-10 px-5 text-indigo-600 transition-colors duration-150 rounded-l bg-indigo-100 focus:shadow-outline hover:bg-indigo-200">
           <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" fillRule="evenodd"></path></svg></button>
         </li>
-        <li><button onClick={next_page} className="mx-1 h-10 px-5 text-indigo-600 transition-colors duration-150 bg-white rounded-r focus:shadow-outline bg-indigo-100">
+        <li><button onClick={next_page} className="mx-1 h-10 px-5 text-indigo-600 transition-colors duration-150 rounded-r bg-indigo-100 focus:shadow-outline hover:bg-indigo-200">
           <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" fillRule="evenodd"></path></svg></button>
         </li>
       </ul>
